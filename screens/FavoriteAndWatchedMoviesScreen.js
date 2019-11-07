7a961de6f2bd3c2dc22e9e072d630bb5f569a1a4
Bastien P <bastien.pinard@ynov.com>
2019-11-07 11:38:17 +0100

    Add fav & search

--- screens/FavoriteAndWatchedMoviesScreen.js
+++ screens/FavoriteAndWatchedMoviesScreen.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, TextInput, ScrollView } from 'react-native';
+import {ScrollViewComponent} from '../components';
 
 class FavoriteAndWatchedMoviesScreen extends React.Component {
   render() {
-    return <Text>I'm the Favorite movie's screen !</Text>;
+    return(
+      <View style={{marginTop:40}}>
+      <ScrollView>
+
+<Text>Favories</Text>
+<View>
+<Text style={styles.categories}>Favories</Text>
+<ScrollViewComponent></ScrollViewComponent>
+
+<Text style={styles.categories}>A voir</Text>
+<ScrollViewComponent></ScrollViewComponent>
+
+</View>
+</ScrollView>
+</View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    height: 200,
+    width: '50%',
+    backgroundColor: 'black',
+    marginLeft: '25%',
+    marginVertical: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textLogo: {
+    fontFamily: 'open-sans',
+    color: 'white',
+    fontSize: 20
+  },
+  categories: {
+    fontSize: 20,
+    fontFamily: 'open-sans-bold',
+    color: 'black',
+    marginLeft: 5
+  }
+});
 
 export default FavoriteAndWatchedMoviesScreen;
