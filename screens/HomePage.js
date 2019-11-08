043d88fb69782fe812771dc048bad345c2ca5d32
chikoure <barkaramy@gmail.com>
2019-11-08 17:35:20 +0100

    fix merge

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Text, View, StyleSheet, ScrollView, Button } from 'react-native';
 import { ScrollViewComponent } from '../components';
 import { connect } from 'react-redux';
 import { withNavigation } from 'react-navigation';
-=======
-import React from "react";
-import { Text, View, StyleSheet, ScrollView, Button } from "react-native";
-import { ScrollViewComponent } from "../components";
-import { connect } from "react-redux";
-import { withNavigation } from "react-navigation";
->>>>>>> origin/master
 
 class HomePage extends React.Component {
   state = {
@@ -26,45 +18,45 @@ class HomePage extends React.Component {
   componentDidMount() {
     this.props.tmdbService
       .getTopRatedMovies()
-      .then(res => {
+      .then((res) => {
         this.setState({ topRatedMovies: res.data });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
 
     this.props.tmdbService
       .getUpcomingMovies()
-      .then(res => {
+      .then((res) => {
         this.setState({ upcomingMovies: res.data });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
 
     this.props.tmdbService
       .getNowPlayingOnTheatherMovies()
-      .then(res => {
+      .then((res) => {
         this.setState({ nowPlayingOnTheatherMovies: res.data });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
 
     this.props.tmdbService
       .getPopularMovies()
-      .then(res => {
+      .then((res) => {
         this.setState({ popularMovies: res.data });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
 
     this.props.tmdbService
       .getPopularTvShow()
-      .then(res => {
+      .then((res) => {
         this.setState({ popularTvShow: res.data });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
 
     this.props.tmdbService
       .getTopRatedTvShow()
-      .then(res => {
+      .then((res) => {
         this.setState({ topRatedTvShow: res.data });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
   render() {
     return (
@@ -76,33 +68,29 @@ class HomePage extends React.Component {
           <View>
             <Text style={styles.categories}>Films les mieux notés</Text>
             <ScrollViewComponent
-              movies={this.state.topRatedMovies}
-            ></ScrollViewComponent>
+              movies={this.state.topRatedMovies}></ScrollViewComponent>
 
             <Text style={styles.categories}>Films à venir</Text>
             <ScrollViewComponent
-              movies={this.state.upcomingMovies}
-            ></ScrollViewComponent>
+              movies={this.state.upcomingMovies}></ScrollViewComponent>
 
             <Text style={styles.categories}>Films au cinéma actuellement</Text>
             <ScrollViewComponent
-              movies={this.state.nowPlayingOnTheatherMovies}
-            ></ScrollViewComponent>
+              movies={
+                this.state.nowPlayingOnTheatherMovies
+              }></ScrollViewComponent>
 
             <Text style={styles.categories}>Films populaires</Text>
             <ScrollViewComponent
-              movies={this.state.popularMovies}
-            ></ScrollViewComponent>
+              movies={this.state.popularMovies}></ScrollViewComponent>
 
             <Text style={styles.categories}>Séries populaires</Text>
             <ScrollViewComponent
-              movies={this.state.popularTvShow}
-            ></ScrollViewComponent>
+              movies={this.state.popularTvShow}></ScrollViewComponent>
 
             <Text style={styles.categories}>Séries les mieux notées</Text>
             <ScrollViewComponent
-              movies={this.state.topRatedTvShow}
-            ></ScrollViewComponent>
+              movies={this.state.topRatedTvShow}></ScrollViewComponent>
           </View>
         </ScrollView>
       </View>
@@ -113,27 +101,27 @@ class HomePage extends React.Component {
 const styles = StyleSheet.create({
   logo: {
     height: 200,
-    width: "50%",
-    backgroundColor: "black",
-    marginLeft: "25%",
+    width: '50%',
+    backgroundColor: 'black',
+    marginLeft: '25%',
     marginVertical: 50,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   textLogo: {
-    fontFamily: "open-sans",
-    color: "white",
+    fontFamily: 'open-sans',
+    color: 'white',
     fontSize: 20
   },
   categories: {
     fontSize: 20,
-    fontFamily: "open-sans-bold",
-    color: "black",
+    fontFamily: 'open-sans-bold',
+    color: 'black',
     marginLeft: 5
   }
 });
 
-const mapStateToProps = stateStore => {
+const mapStateToProps = (stateStore) => {
   return {
     tmdbService: stateStore.serviceReducer.tmdbService
   };
