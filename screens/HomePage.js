def63fea439b2a752a7f07fba6f81957ac4d84c0
Bastien P <bastien.pinard@ynov.com>
2019-11-08 17:29:15 +0100

    add url search tv and movie

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,44 +1,62 @@
-import React from 'react';
-import { Text, View, StyleSheet, ScrollView, Button } from 'react-native';
-import {ScrollViewComponent} from '../components';
-import { connect } from 'react-redux';
-import { withNavigation } from 'react-navigation';
+import React from "react";
+import { Text, View, StyleSheet, ScrollView, Button } from "react-native";
+import { ScrollViewComponent } from "../components";
+import { connect } from "react-redux";
+import { withNavigation } from "react-navigation";
 
 class HomePage extends React.Component {
-  state={
-    topRatedMovies:[],
-    upcomingMovies:[],
-    nowPlayingOnTheatherMovies:[],
-    popularMovies:[],
+  state = {
+    topRatedMovies: [],
+    upcomingMovies: [],
+    nowPlayingOnTheatherMovies: [],
+    popularMovies: [],
 
-    popularTvShow:[],
-    topRatedTvShow:[],
-  }
+    popularTvShow: [],
+    topRatedTvShow: []
+  };
 
-  componentDidMount(){
-    this.props.tmdbService.getTopRatedMovies().then((res)=>{
-        this.setState({topRatedMovies: res.data})
-    }).catch((err)=>console.log(err))
+  componentDidMount() {
+    this.props.tmdbService
+      .getTopRatedMovies()
+      .then(res => {
+        this.setState({ topRatedMovies: res.data });
+      })
+      .catch(err => console.log(err));
 
-    this.props.tmdbService.getUpcomingMovies().then((res)=>{
-      this.setState({upcomingMovies: res.data})
-    }).catch((err)=>console.log(err))
+    this.props.tmdbService
+      .getUpcomingMovies()
+      .then(res => {
+        this.setState({ upcomingMovies: res.data });
+      })
+      .catch(err => console.log(err));
 
-    this.props.tmdbService.getNowPlayingOnTheatherMovies().then((res)=>{
-      this.setState({nowPlayingOnTheatherMovies: res.data})
-    }).catch((err)=>console.log(err))
+    this.props.tmdbService
+      .getNowPlayingOnTheatherMovies()
+      .then(res => {
+        this.setState({ nowPlayingOnTheatherMovies: res.data });
+      })
+      .catch(err => console.log(err));
 
-    this.props.tmdbService.getPopularMovies().then((res)=>{
-      this.setState({popularMovies: res.data})
-    }).catch((err)=>console.log(err))
+    this.props.tmdbService
+      .getPopularMovies()
+      .then(res => {
+        this.setState({ popularMovies: res.data });
+      })
+      .catch(err => console.log(err));
 
-    this.props.tmdbService.getPopularTvShow().then((res)=>{
-      this.setState({popularTvShow: res.data})
-    }).catch((err)=>console.log(err))
+    this.props.tmdbService
+      .getPopularTvShow()
+      .then(res => {
+        this.setState({ popularTvShow: res.data });
+      })
+      .catch(err => console.log(err));
 
-    this.props.tmdbService.getTopRatedTvShow().then((res)=>{
-      this.setState({topRatedTvShow: res.data})
-    }).catch((err)=>console.log(err))
+    this.props.tmdbService
+      .getTopRatedTvShow()
+      .then(res => {
+        this.setState({ topRatedTvShow: res.data });
+      })
+      .catch(err => console.log(err));
   }
   render() {
     return (
@@ -49,22 +67,34 @@ class HomePage extends React.Component {
           </View>
           <View>
             <Text style={styles.categories}>Films les mieux notés</Text>
-            <ScrollViewComponent movies={this.state.topRatedMovies}></ScrollViewComponent>
+            <ScrollViewComponent
+              movies={this.state.topRatedMovies}
+            ></ScrollViewComponent>
 
             <Text style={styles.categories}>Films à venir</Text>
-            <ScrollViewComponent movies={this.state.upcomingMovies}></ScrollViewComponent>
+            <ScrollViewComponent
+              movies={this.state.upcomingMovies}
+            ></ScrollViewComponent>
 
             <Text style={styles.categories}>Films au cinéma actuellement</Text>
-            <ScrollViewComponent movies={this.state.nowPlayingOnTheatherMovies}></ScrollViewComponent>
+            <ScrollViewComponent
+              movies={this.state.nowPlayingOnTheatherMovies}
+            ></ScrollViewComponent>
 
             <Text style={styles.categories}>Films populaires</Text>
-            <ScrollViewComponent movies={this.state.popularMovies}></ScrollViewComponent>
+            <ScrollViewComponent
+              movies={this.state.popularMovies}
+            ></ScrollViewComponent>
 
             <Text style={styles.categories}>Séries populaires</Text>
-            <ScrollViewComponent movies={this.state.popularTvShow}></ScrollViewComponent>
+            <ScrollViewComponent
+              movies={this.state.popularTvShow}
+            ></ScrollViewComponent>
 
             <Text style={styles.categories}>Séries les mieux notées</Text>
-            <ScrollViewComponent movies={this.state.topRatedTvShow}></ScrollViewComponent>
+            <ScrollViewComponent
+              movies={this.state.topRatedTvShow}
+            ></ScrollViewComponent>
           </View>
         </ScrollView>
       </View>
@@ -75,30 +105,30 @@ class HomePage extends React.Component {
 const styles = StyleSheet.create({
   logo: {
     height: 200,
-    width: '50%',
-    backgroundColor: 'black',
-    marginLeft: '25%',
+    width: "50%",
+    backgroundColor: "black",
+    marginLeft: "25%",
     marginVertical: 50,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center"
   },
   textLogo: {
-    fontFamily: 'open-sans',
-    color: 'white',
+    fontFamily: "open-sans",
+    color: "white",
     fontSize: 20
   },
   categories: {
     fontSize: 20,
-    fontFamily: 'open-sans-bold',
-    color: 'black',
+    fontFamily: "open-sans-bold",
+    color: "black",
     marginLeft: 5
   }
 });
 
-const mapStateToProps = (stateStore) => {
-  return ({
-      tmdbService: stateStore.serviceReducer.tmdbService
-  });
-}
+const mapStateToProps = stateStore => {
+  return {
+    tmdbService: stateStore.serviceReducer.tmdbService
+  };
+};
 
 export default withNavigation(connect(mapStateToProps)(HomePage));
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,103 +1,130 @@
-import React from 'react'
-import { Text, View, StyleSheet, TextInput, FlatList, ScrollView, Button } from 'react-native';
-import {ScrollViewComponent} from '../components';
-import { connect } from 'react-redux';
-import { withNavigation } from 'react-navigation';
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  ScrollView,
+  Button
+} from "react-native";
+import { ScrollViewComponent } from "../components";
+import { connect } from "react-redux";
+import { withNavigation } from "react-navigation";
 
-class SearchScreen extends React.Component{
-  state={
-    comedies:[],
-    action:[],
-    documentaries:[],
-    romance:[]
+class SearchScreen extends React.Component {
+  state = {
+    comedies: [],
+    action: [],
+    documentaries: [],
+    romance: []
+  };
+
+  // async getMovie(idCat){
+  //   console.log('oui');
+
+  //   this.tmdb.getMoviesByCategories(idCat).then( (element) => {
+
+  //     console.log('element', element.data);
+  //   })
+
+  // }
+
+  componentDidMount() {
+    //Comédies
+    this.props.tmdbService
+      .getMoviesByCategories(35)
+      .then(res => {
+        this.setState({ comedies: res.data });
+      })
+      .catch(err => console.log(err));
+
+    //Action
+    this.props.tmdbService
+      .getMoviesByCategories(28)
+      .then(res => {
+        this.setState({ action: res.data });
+      })
+      .catch(err => console.log(err));
+
+    //Documentary
+    this.props.tmdbService
+      .getMoviesByCategories(99)
+      .then(res => {
+        this.setState({ documentaries: res.data });
+      })
+      .catch(err => console.log(err));
+
+    //SF
+    this.props.tmdbService
+      .getMoviesByCategories(10749)
+      .then(res => {
+        this.setState({ romance: res.data });
+      })
+      .catch(err => console.log(err));
   }
 
-    // async getMovie(idCat){
-    //   console.log('oui');
-
-    //   this.tmdb.getMoviesByCategories(idCat).then( (element) => {        
-        
-    //     console.log('element', element.data);
-    //   })
-      
-    // }
-
-    componentDidMount(){
-      //Comédies
-      this.props.tmdbService.getMoviesByCategories(35).then((res)=>{
-          this.setState({comedies: res.data})
-      }).catch((err)=>console.log(err))
-
-      //Action
-      this.props.tmdbService.getMoviesByCategories(28).then((res)=>{
-        this.setState({action: res.data})
-    }).catch((err)=>console.log(err))
-
-      //Documentary
-      this.props.tmdbService.getMoviesByCategories(99).then((res)=>{
-        this.setState({documentaries: res.data})
-    }).catch((err)=>console.log(err))
-
-      //SF
-      this.props.tmdbService.getMoviesByCategories(10749).then((res)=>{
-        this.setState({romance: res.data})
-    }).catch((err)=>console.log(err))
-  }
-
-    render(){      
-        return(
-            <View style={{marginTop:40}}>
-            <ScrollView>
-            <Text>Catégories</Text>
-            <TextInput placeholder = "Entrer le nom de film"></TextInput>
-            <Button title="Rechercher" />
-            <View>
+  render() {
+    return (
+      <View style={{ marginTop: 40 }}>
+        <ScrollView>
+          <Text>Catégories</Text>
+          <TextInput placeholder="Entrer le nom de film"></TextInput>
+          <Button title="Rechercher" />
+          <View>
             <Text style={styles.categories}>Comédie :</Text>
-            <ScrollViewComponent movies={this.state.comedies}></ScrollViewComponent>
+            <ScrollViewComponent
+              movies={this.state.comedies}
+            ></ScrollViewComponent>
 
             <Text style={styles.categories}>Action :</Text>
-            <ScrollViewComponent movies={this.state.action}></ScrollViewComponent>
+            <ScrollViewComponent
+              movies={this.state.action}
+            ></ScrollViewComponent>
 
             <Text style={styles.categories}>Documentaires :</Text>
-            <ScrollViewComponent movies={this.state.documentaries}></ScrollViewComponent>
-            
-            <Text style={styles.categories}>Romance :</Text>
-            <ScrollViewComponent movies={this.state.romance}></ScrollViewComponent>
-            
-            </View>
-            </ScrollView>
-            </View>
-            )
-        }
-    }
-    
-    const styles = StyleSheet.create({
-        logo: {
-            height: 200,
-            width: '50%',
-            backgroundColor: 'black',
-            marginLeft: '25%',
-            marginVertical: 50,
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        textLogo: {
-            fontFamily: 'open-sans',
-            color: 'white',
-            fontSize: 20
-        },
-        categories: {
-            fontSize: 20,
-            fontFamily: 'open-sans-bold',
-            color: 'black',
-            marginLeft: 5
-        }
-    });
+            <ScrollViewComponent
+              movies={this.state.documentaries}
+            ></ScrollViewComponent>
 
-    const mapStateToProps = (stateStore) => {
-      return ({
-          tmdbService: stateStore.serviceReducer.tmdbService
-      });
+            <Text style={styles.categories}>Romance :</Text>
+            <ScrollViewComponent
+              movies={this.state.romance}
+            ></ScrollViewComponent>
+          </View>
+        </ScrollView>
+      </View>
+    );
   }
-  
-  export default withNavigation(connect(mapStateToProps)(SearchScreen))
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    height: 200,
+    width: "50%",
+    backgroundColor: "black",
+    marginLeft: "25%",
+    marginVertical: 50,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textLogo: {
+    fontFamily: "open-sans",
+    color: "white",
+    fontSize: 20
+  },
+  categories: {
+    fontSize: 20,
+    fontFamily: "open-sans-bold",
+    color: "black",
+    marginLeft: 5
+  }
+});
+
+const mapStateToProps = stateStore => {
+  return {
+    tmdbService: stateStore.serviceReducer.tmdbService
+  };
+};
+
+export default withNavigation(connect(mapStateToProps)(SearchScreen));
